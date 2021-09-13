774ea95f5523d69a53dfd800cfc09be6b1d331b2
Fadi Farah <fadifarah112233@gmail.com>
2021-09-13 11:26:14 +0300

    added-PaintingsList-and-PaintingEdit-pages-and-base-classes

---
+++ GroupPaintOnlineWebApp/Client/PagesBase/PaintingEditBase.cs
@@ -0,0 +1,99 @@
+﻿using GroupPaintOnlineWebApp.Shared;
+using GroupPaintOnlineWebApp.Shared.RESTServices.ServicesInterfaces;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace GroupPaintOnlineWebApp.Client.PagesBase
+{
+    public class PaintingEditBase : ComponentBase
+    {
+        //Injections
+        [Inject]
+        public IJSRuntime JsRuntime { get; set; }
+        [Inject]
+        public IPaintingService PaintingService { get; set; }
+        [Inject]
+        public NavigationManager NavManager { get; set; }
+
+
+
+        //RouteUri Parameters
+        [Parameter]
+        public string Id { get; set; }
+
+
+        //Canvas Related Properties
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public bool IsPainting { get; set; }
+        public string ImageURL { get; set; }
+        public string DisplayOtherBox { get; set; }
+        public Painting Painting;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var response = await PaintingService.GetPainting(Id);
+            if (response.StatusCode == HttpStatusCode.NotFound)
+            {
+                NavManager.NavigateTo("/paintingsList");
+            }
+            else
+            {
+                Painting = response.Content.ReadFromJsonAsync<Painting>().Result;
+                DisplayOtherBox = "none";
+                var dimension = await JsRuntime.InvokeAsync<WindowDimension>("getWindowDimensions");
+                await JsRuntime.InvokeVoidAsync("onInitialized");
+                Height = dimension.Height;
+                Width = dimension.Width;
+                await JsRuntime.InvokeVoidAsync("drawImage", Painting.ImageURL);
+                ImageURL = Painting.ImageURL;
+                
+            }
+        }
+
+        public void CanvasOnMouseDown()
+        {
+            IsPainting = true;
+        }
+
+        public async Task CanvasOnMouseUp()
+        {
+            IsPainting = false;
+            ImageURL = await JsRuntime.InvokeAsync<string>("getNewImage");
+            Painting.ImageURL = ImageURL;
+        }
+        public void CanvasOnMouseOut()
+        {
+            IsPainting = false;
+        }
+
+        public void OtherButtonClick()
+        {
+            if (DisplayOtherBox == "none")
+                DisplayOtherBox = "block";
+            else
+                DisplayOtherBox = "none";
+        }
+
+        public async Task SaveButtonClick()
+        {
+            var response=await PaintingService.PutPainting(Id, Painting);
+            if (response.IsSuccessStatusCode)
+            {
+                NavManager.NavigateTo("paintingsList");
+            }
+        }
+
+        public class WindowDimension
+        {
+            public int Width { get; set; }
+            public int Height { get; set; }
+        }
+    }
+}
---
+++ GroupPaintOnlineWebApp/Client/PagesBase/PaintingsListBase.cs
@@ -0,0 +1,60 @@
+﻿using GroupPaintOnlineWebApp.Shared;
+using GroupPaintOnlineWebApp.Shared.RESTServices.ServicesInterfaces;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupPaintOnlineWebApp.Client.PagesBase
+{
+    public class PaintingsListBase : ComponentBase
+    {
+        [Inject]
+        public IPaintingService PaintingService { get; set; }
+        [Inject]
+        public NavigationManager NavManager { get; set; }
+        [Inject]
+        public IJSRuntime JsRuntime { get; set; }
+
+        public List<Painting> Paintings { get; set; }
+        public string URL { get; set; }
+        public string DisplayImage { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            try
+            {
+                Paintings = (await PaintingService.GetPaintings()).ToList();
+                DisplayImage = "none";
+            }
+            catch (AccessTokenNotAvailableException exception)
+            {
+                exception.Redirect();
+
+            }
+        }
+        public void ViewImageClick(string imageURL)
+        {
+            URL = imageURL;
+            DisplayImage = "block";
+        }
+        public void CloseImageView()
+        {
+            URL = "";
+            DisplayImage = "none";
+        }
+        public async Task DeletePaintingClick(string Id)
+        {
+            var response = await PaintingService.DeletePainting(Id);
+            if (response.IsSuccessStatusCode)
+            {
+                Paintings = (await PaintingService.GetPaintings()).ToList();
+                StateHasChanged();
+            }
+        }
+    }
+}
